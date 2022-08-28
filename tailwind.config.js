bf19305d9c86132718857226f9942163dad56bab
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-08-29 01:38:33 +0200

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
